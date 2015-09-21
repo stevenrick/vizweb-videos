c46df8e019e5b81e5b3b99a5ab1aeb065fadf39c
Steven Rick <srick@ucsd.edu>
2015-09-21 10:06:08 -0700

    updates ImageBatchAnalyzer to produce .csv output of resutls

--- src/test/java/org/vizweb/examples/ImageBatchAnalyzer.java
+++ src/test/java/org/vizweb/examples/ImageBatchAnalyzer.java
@@ -2,6 +2,7 @@ package org.vizweb.examples;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
@@ -22,7 +23,32 @@ public class ImageBatchAnalyzer {
 		File folder = new File("C:\\Users\\Stoves\\Desktop\\image_analysis");
 		File[] files = folder.listFiles();
 		
+		String output = "C:\\Users\\Stoves\\Desktop\\image_analysis\\results.csv";
+		FileWriter writer = new FileWriter(output);
+		 
+	    writer.append("Image");
+	    writer.append(',');
+	    writer.append("Colorfulness_1");
+	    writer.append(',');
+	    writer.append("Colorfulness_2");
+	    writer.append(',');
+	    writer.append("Leaves");
+	    writer.append(',');
+	    writer.append("Groups_Text");
+	    writer.append(',');
+	    writer.append("H_Balance");
+	    writer.append(',');
+	    writer.append("H_Symm");
+	    writer.append(',');
+	    writer.append("V_Balance");
+	    writer.append(',');
+	    writer.append("V_Symm");
+	    writer.append('\n');
+	    writer.flush();
+		
 		for (File file : files) {
+			
+			System.out.println("Processing: " + file.getName());
 			
 			BufferedImage input = ImageIO.read(file);
 
@@ -37,22 +63,25 @@ public class ImageBatchAnalyzer {
 //			System.out.println("Average Hue: " + Arrays.toString(hue));
 			
 			double col = ColorFeatureComputer.computeColorfulness(input);
-			System.out.println("Colorfulness (method 1): " + col);
+			//System.out.println("Colorfulness (method 1): " + col);
 
 			double col2 = ColorFeatureComputer.computeColorfulness2(input);
-			System.out.println("Colorfulness (method 2): " + col2);
+			//System.out.println("Colorfulness (method 2): " + col2);
 
 			/*******************************
 			 * Compute xy decomposition features (it may take a while)
 			 *******************************/
 			Block root = XYFeatureComputer.getXYBlockStructure(input);
 			
-			ImageIO.write(root.toImage(input), "png",
-					new File("C:\\Users\\Stoves\\Desktop\\image_analysis\\"+
-							file.getName().replaceFirst("[.][^.]+$", "")+"_decomp.png"));
+//			ImageIO.write(root.toImage(input), "png",
+//					new File("C:\\Users\\Stoves\\Desktop\\image_analysis\\"+
+//							file.getName().replaceFirst("[.][^.]+$", "")+"_decomp.png"));
 			
-			System.out.println("Number of leaves: " + XYFeatureComputer.countNumberOfLeaves(root));
-			System.out.println("Number of group of text: " + XYFeatureComputer.countNumberOfTextGroup(root));
+			int leaves = XYFeatureComputer.countNumberOfLeaves(root);
+			//System.out.println("Number of leaves: " + leaves);
+			
+			int textGroups = XYFeatureComputer.countNumberOfTextGroup(root);
+			//System.out.println("Number of group of text: " + textGroups);
 
 			/*******************************
 			 * Compute quadtree features
@@ -60,17 +89,40 @@ public class ImageBatchAnalyzer {
 			Quadtree qtColor = QuadtreeFeatureComputer.getQuadtreeColorEntropy(input);
 			
 			double hBalance = QuadtreeFeatureComputer.computeHorizontalBalance(qtColor);
-			System.out.println("Horizontal Balance: " + hBalance);
+			//System.out.println("Horizontal Balance: " + hBalance);
 			
 			double hSymmetry = QuadtreeFeatureComputer.computeHorizontalSymmetry(qtColor);
-			System.out.println("Horizontal Symmetry: " + hSymmetry);
+			//System.out.println("Horizontal Symmetry: " + hSymmetry);
 			
 			double vBalance = QuadtreeFeatureComputer.computeVerticalBalance(qtColor);
-			System.out.println("Vertical Balance: " + vBalance);
+			//System.out.println("Vertical Balance: " + vBalance);
 			
 			double vSymmetry = QuadtreeFeatureComputer.computeVerticalSymmetry(qtColor);
-			System.out.println("Vertical Symmetry: " + vSymmetry);
+			//System.out.println("Vertical Symmetry: " + vSymmetry);
+			
+		    writer.append(""+file.getName());
+		    writer.append(',');
+		    writer.append(""+col);
+		    writer.append(',');
+		    writer.append(""+col2);
+		    writer.append(',');
+		    writer.append(""+leaves);
+		    writer.append(',');
+		    writer.append(""+textGroups);
+		    writer.append(',');
+		    writer.append(""+hBalance);
+		    writer.append(',');
+		    writer.append(""+hSymmetry);
+		    writer.append(',');
+		    writer.append(""+vBalance);
+		    writer.append(',');
+		    writer.append(""+vSymmetry);
+		    writer.append('\n');
+		    writer.flush();
+			
 		}
+		
+	    writer.close();
 		
 	}
 
